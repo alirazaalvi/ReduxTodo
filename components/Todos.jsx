3ae193812a343862bea0c534be3b01923c4b0efa
Ali Raza <ali.raza@ethosplc.com>
2016-04-17 11:23:05 +0400

    Initial Commit

---
+++ components/Todos.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo, deleteTodo } from '../actions';
+
+class Todos extends React.Component {
+  onChange(e) {
+      const { dispatch } = this.props
+      if(e.keyCode == 13){
+          dispatch(addTodo(e.target.value))
+          e.target.value = ''
+        }
+  }
+
+  render() {
+    const { todos, dispatch } = this.props
+    return( 
+      <div>
+        <h1>Todo List</h1>
+        <div>
+          <h3>New</h3>
+          <input type="text" onKeyUp={this.onChange.bind(this)}/>
+        </div>
+
+        {todos.map((todo, index) => <p key={index}>{todo} <button onClick={e => {
+          dispatch(deleteTodo(index))
+        }}>X</button></p>)}
+    </div>);
+  }
+}
+
+function mapStateToProps(todos) {
+  return {
+    todos
+  }
+}
+
+export default connect(mapStateToProps)(Todos)
